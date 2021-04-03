414e03df9964f07eafa13c23ffb79c99cf3b853b
Ruslan Farkhutdinov <fr3ddy4@yandex.ru>
2021-04-03 20:13:10 +0300

    Added base components

---
+++ src/components/ReportDownload/index.tsx
@@ -0,0 +1,20 @@
+import { CSVLink } from 'react-csv';
+
+export const ReportDownload = () => {
+  const headers = [
+    { label: 'First Name', key: 'firstname' },
+    { label: 'Last Name', key: 'lastname' },
+    { label: 'Email', key: 'email' },
+  ];
+
+  const data = [
+    { firstname: 'Ahmed', lastname: 'Tomi', email: 'ah@smthing.co.com' },
+    { firstname: 'Raed', lastname: 'Labes', email: 'rl@smthing.co.com' },
+    { firstname: 'Yezzi', lastname: 'Min l3b', email: 'ymin@cocococo.com' },
+  ];
+  return (
+    <CSVLink data={data} headers={headers}>
+      Download me
+    </CSVLink>
+  );
+};
--- src/pages/Report/index.tsx
+++ src/pages/Report/index.tsx
@@ -1,5 +1,14 @@
+import { DatePicker } from 'antd';
+import { ReportDownload } from '../../components/ReportDownload';
+
 const Report: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      Report
+      <DatePicker />
+      <ReportDownload />
+    </>
+  );
 };
 
 export default Report;
